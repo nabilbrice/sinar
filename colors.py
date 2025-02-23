44f779cf74c9464fd5e658727bd0908c2d0170f2
Nabil Brice <nabilbrice@github.com>
2025-02-23 17:26:22 +0000

    enabled control of chequered_surface number of boxes in both u,v

--- colors.py
+++ colors.py
@@ -2,8 +2,11 @@ import jax
 from jax import Array
 import jax.numpy as jnp
 
-def chequered_surface(uv: Array) -> Array:
-    case = jnp.sum(jnp.floor(uv * 10), axis=-1) % 2
+# TODO: Remove hardcoded functionality:
+# - Bright color, dark color
+def chequered_surface(uv: Array,
+                      boxes: int | Array = jnp.array([30, 30])) -> Array:
+    case = jnp.sum(jnp.floor(uv * boxes), axis=-1) % 2
     bright = jnp.array([1.0, 0.3, 1.0])
     dark = jnp.array([0.1, 0.1, 0.5])
     return jax.lax.select(case == 0, bright, dark)
